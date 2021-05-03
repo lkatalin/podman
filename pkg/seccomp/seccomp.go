6b31414e7060df760b30170dbeaa88e2b679878f
Lily Sturmann <lsturman@redhat.com>
2021-05-03 14:40:03 -0400

    Add seccompare function and unit tests
    Signed-off-by: Lily Sturmann <lsturman@redhat.com>

--- pkg/seccomp/seccomp.go
+++ pkg/seccomp/seccomp.go
@@ -4,6 +4,9 @@ import (
 	"sort"
 
 	"github.com/pkg/errors"
+	specs "github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/google/go-cmp/cmp"
+	"github.com/sirupsen/logrus"
 )
 
 // ContainerImageLabel is the key of the image annotation embedding a seccomp
@@ -51,4 +54,56 @@ func LookupPolicy(s string) (Policy, error) {
 	sort.Strings(keys)
 
 	return -1, errors.Errorf("invalid seccomp policy %q: valid policies are %+q", s, keys)
+}
+
+// This function compares the seccomp profile of an image against a default
+// seccomp profile and returns the image profile iff it is a strict subset of
+// the default profile. A strict subset means:
+// 1. Any syscalls allowed by the image profile must also be allowed in the default
+//    with the same parameters.
+// 2. The default profile includes more syscalls than the image profile.
+func compare(dflt specs.LinuxSeccomp, img specs.LinuxSeccomp) specs.LinuxSeccomp {
+        // Create mapping of syscalls -> rules from both profiles.
+        var defaultProfile = make(map[string]specs.LinuxSyscall)
+        var imgProfile = make (map[string]specs.LinuxSyscall)
+        for _, rule := range dflt.Syscalls {
+                for _, name := range rule.Names {
+                        defaultProfile[name] = rule
+                }
+        }
+        for _, rule := range img.Syscalls {
+                for _, name := range rule.Names {
+                        imgProfile[name] = rule
+                }
+        }
+
+        // Check whether set of syscalls in default is larger than the set in img;
+        // if not, it won't be a strict subset, so return default.
+        var allPresent = true
+        for syscall := range defaultProfile {
+                _, ok := imgProfile[syscall]
+                if !ok {
+                        allPresent = false
+                }
+        }
+        if allPresent {
+                logrus.Debugf("img seccomp profile is not strict subset; returning default")
+                return dflt
+        }
+
+        // Check that all rules for img syscalls are the same ones present in the
+        // default profile for that syscall.
+        for syscall, imgRule := range imgProfile {
+                defaultRule := defaultProfile[syscall]
+                // TODO: better iterator over fields?
+                if (!cmp.Equal(defaultRule.Action, imgRule.Action) ||
+                    !cmp.Equal(defaultRule.ErrnoRet, imgRule.ErrnoRet) ||
+                    !cmp.Equal(defaultRule.Args, imgRule.Args)) {
+                     logrus.Debugf("default rule %v not equal to img rule %v for syscall %v; returning default profile\n", defaultRule.Action, imgRule.Action, syscall)
+                        return dflt
+                }
+        }
+
+        logrus.Debugf("img seccomp profile is stricter than default; returning img profile")
+        return img
 }
